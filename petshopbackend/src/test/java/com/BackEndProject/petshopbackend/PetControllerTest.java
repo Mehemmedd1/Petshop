57af79fa0bc027a45a3a9d01643214836a0c7ebf
Mehemmed <yerivar0@gmail.com>
2024-08-09 19:54:45 +0400

    petshop

--- petshopbackend/src/test/java/com/BackEndProject/petshopbackend/PetControllerTest.java
+++ petshopbackend/src/test/java/com/BackEndProject/petshopbackend/PetControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,10 +24,8 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PetController.class)
 @ExtendWith(MockitoExtension.class)
@@ -83,6 +82,24 @@ public class PetControllerTest {
         mockMvc.perform(get("/pet/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].pet").value("Buddy"));
+    }
+    @Test
+    void testDeletePet() throws Exception {
+        // when, then
+        mockMvc.perform(delete("/pet/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Pet not found this id: 1"));
+    }
+    @Test
+    void testUpdatePet() throws Exception{
+            when(petService.updatePet(Mockito.anyLong(),any(Pet.class)))
+                    .thenReturn(pet);
+
+            mockMvc.perform(put("/pet/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"pet\": \"Buddy\"}"))
+                    .andExpect(status().isOk()).andExpect(content().string("Succesfully changed"));
+
     }
 
 }
